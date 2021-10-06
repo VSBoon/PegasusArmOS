e1b1e87ccf62380030e668b6360aafb2c7b34e12
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-10-06 14:02:43 +0200

    feat(trajectory): add speed-limiter
    Changed input for trajectory generation from desired number of sub-
    trajectories and total time to maximum joint- and end-effector velocity
    to calculate the number of sub-configurations.
    
    Written by: V.S. Boon
    Committed on: 06-10-2021
    Version: 0.2.9

--- raspberry_pi/trajectory_generation/traj_gen_test.py
+++ raspberry_pi/trajectory_generation/traj_gen_test.py
@@ -15,29 +15,31 @@ sConfigOtherNotSE3 = np.array([[1,0,0,0],
                                [0,1,0,0],
                                [0,0,1,0],
                                [0,0,2,0]])
-tTot = 10
-nSubConfigs = 20
+omgMax = 0.5
+vMax = 0.5
+dt = 0.01
+
 
 
 def test_TrajGenScrewNormal():
-    trajList, timeList = TrajGen(sConfigOther, fConfigOther, tTot, 
-    nSubConfigs, method='screw')
+    trajList, timeList = TrajGen(sConfigOther, fConfigOther, omgMax, 
+    vMax, dt, method='screw')
     assert isinstance(trajList[0], np.ndarray)
     assert isinstance(timeList[0], float)
     assert (trajList[0] == sConfigOther).all()
     assert (trajList[-1] == fConfigOther).all()
 
 def test_TrajGenCartNormal():
-    trajList, timeList = TrajGen(sConfigOther, fConfigOther, tTot, 
-    nSubConfigs, method='cartesian')
+    trajList, timeList = TrajGen(sConfigOther, fConfigOther, omgMax, 
+    vMax, dt, method='cartesian')
     assert isinstance(trajList[0], np.ndarray)
     assert isinstance(timeList[0], float)
     assert (trajList[0] == sConfigOther).all()
     assert (trajList[-1] == fConfigOther).all()
 
 def test_TrajGenJointNormal():
-    trajList, timeList = TrajGen(sConfigJoint, fConfigJoint, tTot, 
-    nSubConfigs, method='joint')
+    trajList, timeList = TrajGen(sConfigJoint, fConfigJoint, omgMax, 
+    vMax, dt, method='joint')
     assert isinstance(trajList[0], np.ndarray)
     assert isinstance(timeList[0], float)
     assert (trajList[0] == sConfigJoint).all()
@@ -45,30 +47,30 @@ def test_TrajGenJointNormal():
 
 def test_TrajGenScrewNotSE3():
     try:
-        trajList, timeList = TrajGen(sConfigOtherNotSE3, fConfigOther, tTot, 
-        nSubConfigs, method='cartesian') 
+        trajList, timeList = TrajGen(sConfigOtherNotSE3, fConfigOther, omgMax, 
+        vMax, dt, method='cartesian') 
     except SyntaxError as e:
         if "input SE(3)" in e.msg:
             assert True 
 
 def test_TrajGenCartNotNpArr():
     try:
-        trajList, timeList = TrajGen(sConfigJoint, fConfigOther, tTot, 
-        nSubConfigs, method='cartesian') 
+        trajList, timeList = TrajGen(sConfigJoint, fConfigOther, omgMax, 
+        vMax, dt, method='cartesian') 
     except SyntaxError as e:
         if "numpy arrays" in e.msg:
             assert True 
 
 def test_TrajGenJointNotList():
     try:
-        trajList, timeList = TrajGen(sConfigJoint, fConfigOther, tTot, 
-        nSubConfigs, method='joint') 
+        trajList, timeList = TrajGen(sConfigJoint, fConfigOther, omgMax, 
+        vMax, dt, method='joint') 
     except SyntaxError as e:
         if "list of joint angles" in e.msg:
             assert True 
 
 if __name__ == "__main__":
-    trajList, timeList = TrajGen(sConfigOther, fConfigOther, tTot, 
-    nSubConfigs, method='cartesian') 
+    trajList, timeList = TrajGen(sConfigOther, fConfigOther, omgMax, 
+    vMax, dt, method='cartesian') 
     print(trajList[3])
     print(timeList[3])
