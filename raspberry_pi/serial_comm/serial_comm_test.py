0de2cd0e38484e568871d326dfd5df960776c2ae
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-10-19 06:57:09 +0200

    test(comms): add FindSerial() test cases, to be run iteratively
    Written by: V.S. Boon
    Committed on: 19-10-2021
    Version: 0.3.11

---
+++ raspberry_pi/serial_comm/serial_comm_test.py
@@ -0,0 +1,41 @@
+from serial_comm import FindSerial, StartComms, GetComms, SReadAndParse, SetPointControl1
+from classes import SerialData, InputError
+
+"""Trying to do multiple test cases for FindSerial() is difficult, 
+since all are run in one go, and the Arduino cannot be connected and 
+disconnected simultaneously. Therefore, uncomment the test that can be 
+done and iterate through them.
+"""
+
+# def test_FindSerialNoPort():
+#     """Test to see if an IOError gets raised when no Arduino is 
+#     connected.
+#     """
+#     try:
+#         FindSerial()
+#     except IOError:
+#         assert True
+
+
+def test_FindSerial1ArdWindows():
+    """Check if the serial port is found on a Windows PC: 
+    edit knownPort before testing!
+    """
+    knownPort = 'COM11'
+    port = FindSerial()[0]
+    assert port == knownPort
+
+# def test_FindSerial1ArdLinux():
+#     """Check if the serial port is found on a Linux PC: 
+#     edit knownPort before testing!
+#     """
+#     knownPort = '/dev/ttyACM0'
+#     port = FindSerial()[0]
+#     assert port == knownPort
+
+# def test_FindSerial2Ard():
+#     """Check if the user is warned bout multiple Arduinos being 
+#     connected.
+#     """
+#     port, warning = FindSerial()
+#     assert warning
