e2a46b0a582b4ae5d22e10e97e1c2a96b500992c
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-11-29 18:47:42 +0200

    fix(robot): update robot parameters
    Written by: V.S. Boon
    Committed on: 29-11-2021
    Version: 0.5.7

--- raspberry_pi/serial_comm/serial_comm_test.py
+++ raspberry_pi/serial_comm/serial_comm_test.py
@@ -9,7 +9,7 @@ import numpy as np
 import serial
 import time
 from serial_comm import FindSerial, StartComms, GetComms, SReadAndParse, SetPointControl1
-from classes import SerialData, InputError, Joint, Link
+from classes import SerialData, InputError, Joint, Link, Robot
 
 """Trying to do multiple test cases for FindSerial() and GetComms() is 
 difficult, since all are run in one go, and the microcontroller cannot 
@@ -144,35 +144,39 @@ def test_SReadAndParseNormal():
 
     ###ROBOT INITIALISATION: NECESSARY FOR SPDATA() INSTANCE###
     #Inertia matrices
-    iMat0 = np.diag([0.03584238, 0.02950513, 0.04859042])
-    iMat1 = np.diag([0.00393345, 0.00236823, 0.00171701])
-    iMat2 = np.diag([0.0092912, 0.00210452, 0.0029424])
-    iMat34 = np.diag([0.00363966, 0.00347835, 0.00041124])
-    massList = [4.99, 0.507, 0.420, 0.952, 0.952]
+    iMat0 = np.diag([0.03947, 0.03362, 0.04886])
+    iMat1 = np.diag([0.00393, 0.00237, 0.00172])
+    iMat2 = np.diag([0.00294, 0.00210, 0.0029])
+    iMat34 = np.diag([0.00041, 0.00348, 0.00364])
+    massList = [5.13, 0.507, 0.420, 0.952, 0.952]
     #Transformation matrices from CoM of links with principle axes of
     #inertia to the space frame (Tsi):
-    Tsi0 = np.array([[0.3804 ,-0.9215,0.0786 ,-0.0103],
-                     [0.8774 ,0.3864 ,0.2843 ,-0.0292],
-                     [-0.2924,-0.0392,0.9555 ,0.0642 ],
-                     [0      ,0      ,0      ,1      ]])
-    Tsi1 = np.array([[-0.0002,0.0008 ,-1.0000,0.0350 ],
-                     [0.4553 ,0.8903 ,0.0007 ,-0.0083],
-                     [0.8903 ,-0.4553,-0.0006,0.1666 ],
-                     [0      ,0      ,0      ,1      ]])
-    Tsi2 = np.array([[-0.9966,0.0819 ,-0.0003,0.0814 ],
-                     [-0.0819,-0.9966,0.0008 ,-0.0083],
-                     [-0.0002,0.0008 ,1.0000 ,0.3550 ],
-                     [0      ,0      ,0      ,1      ]])
-    Tsi34 = np.array([[-0.0326,0.0002 ,-0.9995,0.2640 ],
-                      [-0.0004,1.0000 ,0.0002 ,-0.0059],
-                      [-0.9995,0.0004 ,-0.0326,0.3504 ],
-                      [0      ,0      ,0      ,1      ]])
+    Tsi0 = np.array([[ 0.397, 0.838,-0.375, 0.0284],
+                    [-0.909, 0.416,-0.033,-0.0413],
+                    [ 0.129, 0.354, 0.926, 0.0522],
+                    [0    , 0     , 0    , 1     ]])
+    Tsi1 = np.array([[ 0.000, 0.455, 0.890, 0.0015],
+                    [ 0.001, 0.890,-0.455, 0.0026],
+                    [-1.000, 0.007, 0.001, 0.0039],
+                    [0    , 0     , 0    , 1     ]])
+    Tsi2 = np.array([[-0.003, 0.082, 0.997, 0.0009],
+                    [ 0.001, 0.997,-0.082, 0.0021],
+                    [-1.000, 0.001,-0.003, 0.0029],
+                    [0    , 0     , 0    , 1     ]])
+    Tsi34 = np.array([[-0.999, 0.000, -0.035, 0.0076],
+                    [0.000, -1.000, -0.000,-0.0159],
+                    [-0.035, -0.000, 0.999, 0.5840],
+                    [0    , 0     , 0    , 1     ]])
+    TsbHome = np.array([[1,0,0, 0.1474],
+                        [0,1,0,-0.0168],
+                        [0,0,1, 0.5853],
+                        [0,0,0, 1     ]])
     #Screw axes in the Space Frame {s}
     S0 = np.array([0,0,1,0,0,0])
-    S1 = np.array([0,1,0,-0.0035,0,0.126])
-    S2 = np.array([0,1,0,-0.0035,0,0.335])
-    S3 = np.array([0,1,0,-0.234,0,0.355])
-    S4 = np.array([0,0,1,-0.234,-0.016,0])
+    S1 = np.array([0,1,0,-0.125,0,0.0035])
+    S2 = np.array([0,1,0,-0.355,0,0.0035])
+    S3 = np.array([0,1,0,-0.585,0,0.0030])
+    S4 = np.array([1,0,0,0,0.585,0.016])
     lims0 = [-0.945*np.pi, 0.945*np.pi] #+/- 170 deg
     lims1 = [-0.25*np.pi, 0.5*np.pi] #-45 deg, + 90 deg
     lims2 = [-np.pi, 0.25*np.pi] #-180 deg, + 45 deg.
@@ -184,12 +188,16 @@ def test_SReadAndParseNormal():
     L1 = Link(iMat1, massList[1], L0, Tsi1)
     L2 = Link(iMat2, massList[2], L1, Tsi2)
     L34 = Link(iMat34, massList[3], L2, Tsi34)
-    J0 = Joint(S0, [None, L0], gearRatioList[0], cpr, lims0)
-    J1 = Joint(S1, [L0, L1], gearRatioList[1], cpr, lims1)
-    J2 = Joint(S2, [L1, L2], gearRatioList[2], cpr, lims2)
-    J3 = Joint(S3, [L2,34], gearRatioList[3], cpr, lims3)
-    J4 = Joint(S4, [L2,L34], gearRatioList[4], cpr, lims4)
+    links = [L0, L1, L2, L34, L34]
+    km = [22.7*10**(-3), 22.7*10**(-3), 22.7*10**(-3), 22.7*10**(-3),
+            22.7*10**(-3), 22.7*10**(-3), 9.2*10**(-3)] 
+    J0 = Joint(S0, [None, L0], gearRatioList[0], km[0], cpr, lims0)
+    J1 = Joint(S1, [L0, L1], gearRatioList[1], km[1], cpr, lims1)
+    J2 = Joint(S2, [L1, L2], gearRatioList[2], km[2], cpr, lims2)
+    J3 = Joint(S3, [L2,L34], gearRatioList[3], km[3], cpr, lims3)
+    J4 = Joint(S4, [L2,L34], gearRatioList[4], km[4], cpr, lims4)
     joints = [J0, J1, J2, J3, J4]
+    robot = Robot(joints, links, TsbHome)
     ###END OF ROBOT INITIALISATION###
 
     SPData = SerialData(lenData, desAngles, maxDeltaAngles, tolAngle, joints)
