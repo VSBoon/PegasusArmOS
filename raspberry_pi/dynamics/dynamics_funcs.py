f028f81d4a432dd1b409d66fe9100b2f32fe6602
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-11-29 17:05:19 +0200

    fix: inverse dynamics functional
    I rewrote the inverse dynamics function from a lower-level (deeper theory), to avoid black-box behaviour and improve efficiency.
    
    Written by: V.S. Boon
    Committed on: 29-11-2021
    Version: 0.5.6

--- raspberry_pi/dynamics/dynamics_funcs.py
+++ raspberry_pi/dynamics/dynamics_funcs.py
@@ -45,49 +45,82 @@ def LossComp(tauComm: float, dtheta: float,  tauStat: float,
                    bVisc*dtheta
     return tauComm/eff
 
-def FeedForward(robot: Robot, thetaList: np.ndarray, dthetaList: np.ndarray, 
-                ddthetaList: np.ndarray, Ftip: np.ndarray) -> \
-                np.ndarray:
-    """Computes the feed-forward torque based on the inverse dynamics 
-    of the robot.
-    NOTE: This function is used for robots with a shared fifth link. 
-    Robots having a 'pure' serial configuration can simply use the 
-    modern_robotics.InverseDynamics() function.
-    :param robot: A Robot-class instance containing all characteristic
-                  information of the robot.
-    :param thetaList: Array of currently desired joint angles.
-    :param dthetaList: Array of currently desired joint velocities.
-    :param ddthetaList: Array of currently desired joint accelerations.
-    :param Ftip: 6-vector representing the desired end-effector forces 
-                 and torques in the end-effector frame(!).
-    :return FFTorque: Array of feed-forward joint torques.
+def FeedForward(robot: Robot, theta: List, dtheta: List, ddtheta: List, g: np.ndarray, FTip: np.ndarray) -> List:
+    """Optimized feed-forward for the Pegasus arm, only going through 
+    the Newton-Euler inverse dynamics algorithm once. Based on the
+    Modern Robotics Python Library.
+    :param robot: A Robot class describing the robot mathematically.
+    :param thetaList: List of desired joint angles.
+    :param dthetaList: List of desired joint velocities.
+    :param ddthetaList: List of desired joint accelerations.
+    :param g: 3-dimensional gravity vector in [m/s^2].
+    :param FTip: 6-dimensional End-effector wrench.
+    :return tau: A list of required joint torques at the output shaft.
+
+    Example input:
+    
     """
-    #TODO: checks for lists, checks for Ftip.
-    g = np.array([0, 0, -9.81]) #gravity-vector in-line with the space frame!
-    """Calculate FF torque for all joints except the last, 
-    due to shared link w/ joint 4 (which causes problematic dynamics).
-    """
-    sList = robot.screwAxes[0]
-    for i in range(1, len(robot.screwAxes)-1):
-        sList = np.c_[sList, robot.screwAxes[i]] #Add columns
-    FFTorque = mr.InverseDynamics(thetaList[:-1], dthetaList[:-1], 
-                                  ddthetaList[:-1], g, Ftip, \
-                                  robot.TllList[:-1], robot.GiList[:-1], \
-                                  sList)
-    """Remove joint 4 from the model to calculate final joint torque
-    NOTE: Might be inefficient, as the torques for motor 0 through 3 
-    are calculated twice.
-    """
-    thetaList = np.delete(thetaList, -2)
-    dthetaList = np.delete(dthetaList, -2)
-    ddthetaList = np.delete(ddthetaList, -2)
-    sListFinal = robot.screwAxes[-1]
-    for i in range(len(robot.screwAxes)-2):
-        sListFinal = np.c_[sListFinal, robot.screwAxes[i]]
-    TllFinal = robot.TllList[0:-2] + [robot.TllList[-1]]
-    GiFinal = robot.GiList[0:-2] + [robot.GiList[-1]]
-    lastTorque = mr.InverseDynamics(thetaList, dthetaList, ddthetaList, g, \
-                                    Ftip, TllFinal, GiFinal, \
-                                    sListFinal)[-1]
-    FFTorque = np.append(FFTorque, lastTorque)
-    return FFTorque
+    #Initialization
+    #Note: 6,shape is used to obtain column vectors for calculations.
+    n = len(theta) #Number of joints
+    Tsi = np.eye(4)
+    AdHis = np.zeros((n+1,6,6))
+    screwS = np.c_[robot.screwAxes[0], robot.screwAxes[1]]
+    for i in range(2,n):
+        screwS = np.c_[screwS, robot.screwAxes[i]]
+    screwA = np.zeros((6, n))
+    expT = np.zeros((n,4,4))
+    AdTiiN = np.zeros((n+1,6,6))
+    V = np.zeros((6, n+1))
+    dV = np.zeros((6, n+1))
+    dV[3:,0] = -g #Gravity is equivalent to upward acceleration
+    #This way, the gravity gets 'carried on' in forward iterations.
+    F = np.zeros((6, n+1))
+    F[:, n] = FTip
+    tau = np.zeros(n)
+    #Forward iterations
+    for i in range(n):
+        if i != n-1:
+            #Adjoint of the transformation matrix {i} to {s} in the home config
+            #For some reason, Tsi = robot.links[i].Tsi gives the wrong answer...
+            Tsi = np.dot(Tsi, robot.links[i].Tii)
+            AdHis[i] = mr.Adjoint(mr.TransInv(Tsi))
+            #Screw axis in frame {i}. np.array([]) to go from 1D to 2D
+            screwA[:,i] = np.dot(AdHis[i],screwS[:,i])
+            #Exponential of A with angle theta
+            expT[i] = mr.MatrixExp6(mr.VecTose3(screwA[:,i]*-theta[i]))
+            #Adjoint of T_(i,i-1), given current config:
+            AdTiiN[i] = mr.Adjoint(np.dot(expT[i],mr.TransInv(robot.TllList[i])))
+            #Twist due to joint velocity + twist due to prev. link
+            V[:,i+1] = screwA[:,i]*dtheta[i] + np.dot(AdTiiN[i], V[:,i])
+            #Acceleration due to joint acceleration + vel-prod. term + prev link
+            dV[:,i+1] = screwA[:,i]*ddtheta[i] + np.dot(AdTiiN[i], dV[:,i]) + \
+                    np.dot(mr.ad(V[:,i+1]), screwA[:,i]*dtheta[i])
+        elif i == n-1:
+            AdHis[i] = mr.Adjoint(mr.TransInv(Tsi))
+            screwA[:,i] = np.dot(AdHis[i],screwS[:,i])
+            expT[i] = mr.MatrixExp6(mr.VecTose3(screwA[:,i]*-theta[i]))
+            AdTiiN[i] = mr.Adjoint(np.dot(expT[i],mr.TransInv(robot.TllList[i])))
+            #NOTE: Prev twist == V[:,i-1] & Prev acc == dV[:,i-1]
+            V[:,i+1] = screwA[:,i]*dtheta[i] + np.dot(AdTiiN[i], V[:,i-1])
+            dV[:,i+1] = screwA[:,i]*ddtheta[i] + np.dot(AdTiiN[i], dV[:,i-1]) + \
+                    np.dot(mr.ad(V[:,i+1]), screwA[:,i]*dtheta[i])
+    #Backward iterations
+    for i in range(n-1, -1, -1):
+        if i != n-2:
+            """Wrench due to wrench next link + link acceleration - 
+            Coriolis/centripetal terms"""
+            #AdTiiN is transposed to obtain T_(i,i+1) from T_(i+1,i)
+            F[:,i] = np.dot(AdTiiN[i+1].T, F[:,i+1]) + \
+                np.dot(robot.GiList[i], dV[:,i+1]) - \
+                np.dot(mr.ad(V[:,i+1]).T, np.dot(robot.GiList[i], V[:,i+1]))
+        elif i == n-2: #Pegasus arm specific mechanics
+            #NOTE: Wrench of next 'link' is FTip --> F[:,i+2]
+            F[:,i] = np.dot(AdTiiN[i+1].T, F[:,n]) + \
+                np.dot(robot.GiList[i], dV[:,i+2]) - \
+                np.dot(mr.ad(V[:,i+2]).T, np.dot(robot.GiList[i], V[:,i+2]))
+        """Obtain torques by projection through screwA, effectively a
+        column of the Jacobian between wrenches/twists in {i} and 
+        joint space"""
+        tau[i] = np.dot(F[:,i].T, screwA[:,i]) 
+    return tau
