707f8e33bd771fcb1817e01f3f1ad42b867abaa4
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-12-03 14:47:06 +0200

    feat(ctrl): add force & impedance control
    Written by: V.S. Boon
    Committed on: 03-12-2021
    Version: 0.6.4

--- raspberry_pi/control/control.py
+++ raspberry_pi/control/control.py
@@ -1,23 +1,25 @@
 import os
 import sys
+
 #Find directory path of current file
 current = os.path.dirname(os.path.realpath(__file__))
 #Find directory path of parent folder and add to sys path
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from typing import Union, List
+from typing import Union, List, Tuple
+import modern_robotics as mr
 import numpy as np
 import time
 import serial
-from kinematics.kinematic_funcs import IKSpace
+from kinematics.kinematic_funcs import IKSpace, FKSpace
 from trajectory_generation.traj_gen import TrajGen, TrajDerivatives
 from dynamics.dynamics_funcs import FeedForward
 from serial_comm.serial_comm import SReadAndParse
-from classes import Robot, SerialData, IKAlgorithmError
-from util import PID, Tau2Curr, Curr2MSpeed
+from classes import Robot, SerialData, PID, IKAlgorithmError, InputError
+from util import Tau2Curr, Curr2MSpeed, RToEuler
 
-def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List], robot: Robot, serial: SerialData, dt: float, vMax: float, omgMax: float, kP: float, kI: float, kD: float, dtComm: float, dtPID: float): #Removed localMu for testing
+def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List], robot: Robot, serial: SerialData, dt: float, vMax: float, omgMax: float, PIDObj: PID, dtComm: float, dtPID: float): #Removed localMu for testing
     """Position control by means of point-to-point trajectory 
     generation combined with feed-forward and PID torque control.
     :param sConfig: Start configuration, either in SE(3) or a list of 
@@ -30,9 +32,8 @@ def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List
     :param dt: Time between subconfigurations of the trajectory in [s].
     :param vMax: Maximum linear velocity of the end-effector.
     :param omgMax: Maximum rotational velocity of the joints.
-    :param kP: Proportional constant for PID control.
-    :param kI: Integral constant for PID control.
-    :param kD: Derivative constant for PID control.
+    :param PIDObj: PID class for storage & execution of PID-related 
+                   computations.
     :param dtComm: Interval of sending & receiving data w.r.t the 
                    local microcontroller in [s].
     :param dtPID: Time between PID torque updates in [s].
@@ -49,6 +50,8 @@ def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List
     kP = 1
     kI = 0.01
     kD = 0.01
+    ILim = np.array([5,5,5,5,5])
+    PIDObj = PID(kP, kI, kD, ILim)
     dtComm = 0.5
     dtPID = 0.02
     """
@@ -57,8 +60,10 @@ def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List
     if isinstance(sConfig, np.ndarray):
         if isinstance(eConfig, np.ndarray) and sConfig.shape == eConfig.shape:
             if sConfig.shape == (4,4): #Translate both to joints space 
-                sConfig, successS = IKSpace(robot.TsbHome, sConfig)
-                eConfig, successE = IKSpace(robot.TsbHome, eConfig)
+                sConfig, successS = IKSpace(robot.TsbHome, sConfig,
+                                    robot.screwAxes, robot.limList)
+                eConfig, successE = IKSpace(robot.TsbHome, eConfig, 
+                                    robot.screwAxes, robot.limList)
                 if not successS or not successE:
                     raise IKAlgorithmError()
         else:
@@ -79,15 +84,13 @@ def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List
     traj, velTraj, accTraj = TrajDerivatives(traj, method, robot, dt)
     g = np.array([0,0,-9.81])
     FTip = np.zeros(6) #Position control, --> assume no end-effector force.
-    err = np.zeros(traj[0,:].size)
-    termI = np.zeros(traj[0,:].size)
-    ILim = np.array([1,1,1,1,1])
     tauPID = np.zeros(traj[0,:].size)
     n = -1 #iterator
     startTime = time.perf_counter()
     lastCheck = time.perf_counter()
     lastWrite = time.perf_counter()
     lastPID = time.perf_counter()
+
     print("Starting trajectory...")
     while time.perf_counter() - startTime < dt*traj[:,0].size: #Trajectory loop
         nPrev = n
@@ -98,9 +101,7 @@ def PosControl(sConfig: Union[np.ndarray, List], eConfig: Union[np.ndarray, List
             tauFF = FeedForward(robot, traj[n], velTraj[n], accTraj[n], g, FTip)
         if time.perf_counter() - lastPID >= dtPID:
             thetaCurr = serial.currAngle[:-1] #Exclude gripper data
-            errPrev = err
-            tauPID, termI, err = PID(traj[n], thetaCurr, kP, kI, kD, termI, 
-                                     ILim, dtPID, errPrev)
+            tauPID = PIDObj.Execute(traj[n], thetaCurr, dtPID)
             lastPID = time.perf_counter()
         
         tau = tauFF + tauPID
@@ -159,3 +160,164 @@ def Grip(gripping: bool, serial: SerialData):
     else:
         serial.mSpeed[-1] = 0
 
+def VelControl(robot: Robot, serial: SerialData, vel: 
+               Union[List, np.ndarray], dthetaPrev: np.ndarray, 
+               dt: float, method: str, dtComm: float, dtPID: float, 
+               PIDObj: PID):
+    """Single velocity control loop using feed-forward and PID to 
+    compute the torque required to obtain the desired velocity, either
+    in joint space or end-effector space.
+    :param robot: A Robot object describing the robot mathematically.
+    :param serial: SerialData object for data transmission and 
+                   -storage.
+    :param vel: Desired velocity, either as an end-effector twist or a
+                list/array of joint velocities.
+    :param dthetaPrev: Array of previous velocity.
+    :param dt: Time between this control loop and the previous in [s].
+    :param method: 'twist' if the velocity is a twist, 'joint' for a 
+                   list/array of joint velocities.
+    :param dtComm: Interval between microcontroller communications.
+                   Ensure this value aligns with the value in the 
+                   Teensy's code (in Teensy in [ms], here in [s])!
+    :param dtPID: Interval between PID updates in [s].
+    :param PIDObj: PID class for storage & execution of PID-related 
+                   computations.
+    """
+    vel = np.array(vel)
+    theta = serial.currAngle
+    if method == 'twist' or method == 'Twist':
+        #Calculate joint velocities with pseudo-inverse Jacobian
+        Slist = np.c_[robot.screwAxes[0], robot.screwAxes[1]]
+        for i in range(2, len(robot.screwAxes)):
+            Slist = np.c_[Slist, robot.screwAxes[i]]
+        dtheta = np.dot(np.linalg.pinv(mr.JacobianSpace(Slist, theta)),
+                        vel)
+    elif method == 'joint' or method == 'Joint':
+        dtheta = vel
+    else:
+        raise InputError("Invalid method. Choose between 'joint'" +
+                         "and 'twist'.")
+    ddtheta = (dtheta - dthetaPrev)/dt
+    dthetaCurr = (np.array(serial.currAngle) - 
+                  np.array(serial.prevAngle))/dtComm
+    g = np.array([0,0,-9.81])
+    FTip = np.zeros(6) #Velocity control, no FTip
+    tauFF = FeedForward(robot, theta, dtheta, ddtheta, g, FTip)
+    tauPID = PIDObj.Execute(dtheta, dthetaCurr, dtPID)
+    tau = tauFF + tauPID
+    I = [Tau2Curr(tau[i], robot.joints[i].gearRatio, robot.joints[i].km, 
+                  currLim=2) for i in range(len(robot.joints))]
+    #TODO: Confirm conversion factor I --> PWM
+    PWM = [round(Curr2MSpeed(current)) for current in I]
+    serial.mSpeed[:-1] = PWM
+    #TODO: Add current / PWM for gripper
+
+    
+def ForceControl(robot: Robot, serial: SerialData,
+    FTip: np.ndarray, damping: float, dt: float):
+    """ Feed-forward force control with velocity damping to ensure
+    safety.
+    :param robot: A Robot object describing the robot mathematically.
+    :param serial: SerialData object for data transmission and 
+                   -storage.
+    :param FTip: Desired end-effector wrench
+    :param damping: Gain term to limit velocity of the end-effector.
+    :param dt: Interval between ForceControl updates.
+
+    NOTE: Due to the lack of force sensors, this code is highly 
+    reliant on an accurate model of the robot, which as of writing this
+    code is not exactly the case. Mainly frictional terms and masses 
+    of links should be researched closely before accurate force control
+    can be realised. Alternatively, if one can integrate a force-torque
+    sensor at the 'wrist' of the end-effector, this can be used for PI 
+    feedback control (do not use derivative-control, as explained in 
+    the Modern Robotics book!!!). Lastly, another possibility for more 
+    accurate tau control would be adding integrating strain sensors at
+    the output shafts of the joints for torque feedback, which can 
+    easily be mapped to an end-effector wrench for feedback control.
+    """
+    theta = serial.currAngle
+    g = np.array([0,0,-9.81])
+    dtheta = np.zeros(len(robot.joints))
+    ddtheta = np.zeros(len(robot.joints))
+    tauFF = FeedForward(robot, theta, dtheta, ddtheta, g, FTip)
+    kDamping = np.eye(6)*damping
+    dthetaCurr = (theta - serial.prevAngle)/dt
+    Slist = np.c_[robot.screwAxes[0], robot.screwAxes[1]]
+    for i in range(2, len(robot.screwAxes)):
+        Slist = np.c_[Slist, robot.screwAxes[i]]
+    twist = np.dot(mr.JacobianSpace(Slist, theta), dthetaCurr)
+    """The final term in tau prevents the end-effector from quickly
+    moving / accelerating when it is not pushing against anything."""
+    tau = tauFF - np.dot(kDamping, twist) 
+    I = [Tau2Curr(tau[i], robot.joints[i].gearRatio, robot.joints[i].km, 
+                  currLim=2) for i in range(len(robot.joints))]
+    #TODO: Confirm conversion factor I --> PWM
+    PWM = [round(Curr2MSpeed(current)) for current in I]
+    serial.mSpeed[:-1] = PWM
+    #TODO: Add current / PWM for gripper
+
+def ImpControl(robot: Robot, serial: SerialData, TDes: np.ndarray,
+               VPrev: np.ndarray, dthetaPrev: np.ndarray, dt: float, 
+               M: np.ndarray, B: np.ndarray, Kx: np.ndarray, 
+               Ka: np.ndarray, PIDObj: PID):
+    """Impedance control loop in the end-effector space.
+    :param robot: A Robot object describing the robot mathematically.
+    :param serial: SerialData object for data transmission and 
+                   -storage.
+    :TDes: Desired end-effector configuration as an SE(3) 
+           transformation matrix.
+    :param VPrev: Previous end-effector twist.
+    :pram dthetaPrev: Array of previous joint velocities.
+    :param dt: time between two calls of ImpControl in [s].
+    :param M: Positive-definite 6x6 impedance mass matrix
+    :param B: Positive-definite 6x6 impedance damping matrix.
+    :param Kx: Postive-definite 3x3 impedance linear spring matrix.
+    :param Ka: 3x3 impedance rotational spring matrix."""
+    """Determine position & end-effector angles relative to the desired 
+    configuration."""
+    R,posDes = mr.TransToRp(TDes)
+    thetaDes = IKSpace(robot.TsbHome, TDes, robot.screwAxes, robot.limList)
+    anglesDes = RToEuler(R)
+    TCurr = FKSpace(robot.TsbHome, robot.screwAxes, serial.currAngle[:-1])
+    RCurr, posCurr = mr.TransToRp(TCurr)
+    anglesCurr = RToEuler(RCurr)
+    pos = posCurr - posDes
+    angles = anglesCurr - anglesDes
+
+    """Obtain the error twist and derivative of the error twist"""
+    theta = serial.currAngle
+    dtheta = (serial.currAngle - serial.prevAngle)/dt
+    Slist = np.c_[robot.screwAxes[0], robot.screwAxes[1]]
+    for i in range(2, len(robot.screwAxes)):
+        Slist = np.c_[Slist, robot.screwAxes[i]]
+    V = np.dot(mr.JacobianSpace(Slist, theta), dtheta)
+    dV = (V - VPrev)/dt
+    ddtheta = (dtheta - dthetaPrev)/dt
+    g = np.array([0,0,-9.81])
+    FlinK = np.zeros(6)
+    FlinK[0:3] = np.dot(Kx, pos)
+    FrotK = np.zeros(6)
+    FrotK[0:3] = np.dot(Ka, angles)
+    FK = (FlinK + FrotK)
+    #Simulate a mass-spring-damper system at the end-effector
+    FTip = -(np.dot(M,dV) + np.dot(B,V) + FK)
+    ###EXPERIMENTAL PID, uncomment at own risk###
+    # if np.allclose(FTip, np.zeros(6), atol=0.01): #FTip has stabilized
+    #     #NOTE: atol might need tweaking!
+    #     FTip = np.zeros(6)
+    #     #PID in theta-space
+    #     tauPID = PIDObj.Execute(thetaDes, serial.currAngle, dt)
+    # else:
+    #     PIDObj.errPrev = 0
+    #     PIDObj.termI = np.zeros(len(robot.joints))
+    #     tauPID = 0
+    tauFF = FeedForward(robot, theta, dtheta, ddtheta, g, FTip)
+    tauPID = np.zeros(len(tauFF)) #Remove if experimental PID is uncommented
+    tau = tauFF + tauPID
+    I = [Tau2Curr(tau[i], robot.joints[i].gearRatio, robot.joints[i].km, 
+                  currLim=2) for i in range(len(robot.joints))]
+    #TODO: Confirm conversion factor I --> PWM
+    PWM = [round(Curr2MSpeed(current)) for current in I]
+    serial.mSpeed[:-1] = PWM
+    #TODO: Add current / PWM for gripper
