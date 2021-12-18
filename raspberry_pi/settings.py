53308ac6840eaaa05e809159b4ea9ead84451d21
SoulSurvivor-Boon <62327041+SoulSurvivor-Boon@users.noreply.github.com>
2021-12-18 21:50:36 +0200

    feat(docs): explanations for settings
    Written by: V.S. Boon
    Committed on: 18-12-2021
    Version: 0.7.3

--- raspberry_pi/settings.py
+++ raspberry_pi/settings.py
@@ -2,30 +2,63 @@ import numpy as np
 from classes import PID
 """This document contains all settings that are unrelated to the robot 
 model. For the robot model, see robot_init.py. If you desire to change 
-settings related to main.py, kindly do so here. """
+settings related to main.py, kindly do so here and in robot_init.py."""
 sett = dict()
+#Time between refreshing frames for the UI [s].
 sett['dtFrame'] = 0.1
-sett['dtPID'] = 0.1
-sett['dtComm'] = 0.1 #Ensure equal to one in Teensy code.
-sett['dtFF'] = 0.1
-sett['dtPosConfig'] = sett['dtFF']
+#Time between PID updates. Note: higher dtPID leads to more instability! [s].
+sett['dtPID'] = 0.05
+#Data communication interval with Teensy [s].
+sett['dtComm'] = 0.05 #Ensure equal to one in Teensy code [s].
+#Time between FF updates [s].
+sett['dtFF'] = 0.05 
+#Time interval between trajectory sub-configurations [s].
+sett['dtPosConfig'] = 0.05
+#Interval for updating the FF & PID while holding a position [s].
 sett['dtHold'] = sett['dtFF']
-sett['vMax'] = 0.2
-sett['wMax'] = 0.3*np.pi
-sett['kP'] = np.diag(np.array([8,15,0,0,0]))
-sett['kI'] = np.diag(np.array([0,0,0,0,0]))
-sett['kD'] = np.diag(np.array([0,0,0,0,0]))
-#sett['kP2'] = np.diag(np.array([5,5,10,0,0]))
-#sett['kI2'] = np.diag(np.array([0.02,0.02,0.02,0,0]))
-#sett['kD2'] = np.diag(np.array([1,1,1,0,0]))
-sett['PID'] = PID(sett['kP'], sett['kI'], sett['kD'], ILim=np.array([5,5,5,5,5]))
-#sett['PIDVel'] = PID(sett['kP2'], sett['kI2'], sett['kD2'], ILim=np.array([5,5,5,5,5]))
-sett['jIncr'] = 0.05*np.pi
+#Maximum linear velocity of the end-effector in position control [m/s].
+sett['vMax'] = 0.01
+#Maximum rotational velocity of the joints in position control [rad/s].
+sett['wMax'] = 0.04*np.pi
+#Proportional gain for position control [(N/m)/rad].
+sett['kPP'] = np.diag(np.array([14,22,28,6,6]))
+#Integral gain for position control [(N/m)*s/rad].
+sett['kIP'] = np.diag(np.array([4,6,10,6,6]))
+#Derivative gain for position control [(N/m)/(rad*s)].
+sett['kDP'] = np.diag(np.array([2,2,4,3,3]))
+#Proportional gain for velocity control [(N/m)/(rad/s)].
+sett['kPV'] = np.diag(np.array([0,0,0,0,0]))
+#Integral gain for velocity control [(N/m)*s/(rad/s)].
+sett['kIV'] = np.diag(np.array([0,0,0,0,0]))
+#Derivative gain for velocity control [(N/m)/(rad)].
+sett['kDV'] = np.diag(np.array([0,0,0,0,0]))
+#PID object for position control.
+sett['PIDP'] = PID(sett['kPP'], sett['kIP'], sett['kDP'], ILim=np.array([7,7,7,7,7]))
+#PID object for velocity control
+sett['PIDV'] = PID(sett['kPV'], sett['kIV'], sett['kDV'], ILim=np.array([5,5,5,5,5]))
+#Joint speed increase for joint velocity control when pressing the 
+#'increase' button [rad/s].
+sett['jIncr'] = 0.02*np.pi
+#Linear end-effector speed increase for end-effector velocity control
+#when pressing the 'increase' button [m/s].
 sett['eIncrLin'] = 0.05
+#Rotational end-effector speed increase for end-effector velocity 
+#control when pressing the 'increase' button [rad/s]
 sett['eIncrRot'] = 0.05
-sett['errThetaHold'] = 0.02*np.pi
+#Maximum allowed joint angle error for position control [rad].
+sett['errThetaHold'] = 0.017*np.pi
+#Virtual damper for force control to avoid high end-effector velocities
+#when the end-effector cannot push against an object [(N/m)/(rad/s)].
 sett['forceDamp'] = 100
+#Virtual spatial inertia matrix (inertia+mass) matrix at the end-effector 
+#for impedance control [kg*m^2 & kg].
 sett['M'] = 0.2*np.eye(6)
+#Virtual end-effector damper for impedance control [(N/m)/(rad/s) & N/(m/s)]
 sett['B'] = 10*np.eye(6)
+#Virtual linear spring for impedance control [N/m]
 sett['Kx'] = 100*np.eye(3)
+#Virtual rotational spring for impedance control [(N/m)/rad]
 sett['Ka'] = 10*np.eye(3)
+#Additional virtual damping coefficient to avoid high end-effector 
+#velocities [(N/m)/(rad/s)]
+sett['D'] = 4
